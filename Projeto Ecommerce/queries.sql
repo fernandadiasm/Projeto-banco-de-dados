573d0fe15309e66951c0cf1f41854935d205aa68
Fernanda.dias97@hotmail.com <fernanda.dias97@hotmail.com>
2022-09-09 00:35:25 -0300

    adiciona as queries do projeto ecommerce

---
+++ Projeto Ecommerce/queries.sql
@@ -0,0 +1,129 @@
+-- criação do banco de dados para o cenário de e-commerce
+
+create database ecommerce;
+use ecommerce;
+
+-- tabela de cliente
+-- autoincrementação gera um id automático
+create table clients (
+	idClient int auto_increment primary key,
+    email varchar(30),
+    first_name varchar(30) not null,
+    middle_name varchar(30),
+    last_name varchar(30) not null,
+    CPF char(11) not null,
+    date_of_birthday date not null,
+    gender char(5) check (gender in ('F','M','Outro')),
+    phone_number int,
+    created_at timestamp,
+    modified_at timestamp,
+    constraint unique_cpf_client unique (CPF)
+);
+
+
+-- tabela de endereço do cliente
+create table client_address(
+	idAddress int auto_increment primary key, 
+    idClient int,
+    address_line1 varchar(10) not null,
+    address_line2 varchar(10),
+    city varchar(10) not null,
+    postal_Code varchar(10) not null,
+    country varchar(10) not null,
+    constraint fk_client_address foreign key (idclient) references clients (idClient)
+);
+
+
+
+-- tabela de pagamento
+create table client_payment (
+	idpayment int auto_increment primary key,
+    idClient int,
+    payment_type enum('Boleto','Cartão','Dois cartões', 'Pix'), 
+    Payment_value decimal (5,2),
+	constraint fk_client foreign key (idclient) references clients (idClient)
+    );
+    
+    desc client_payment;
+    
+-- tabela produto
+-- size equivale a dimensão do produto
+create table product (
+	idProduct int auto_increment primary key,
+    name_product varchar(30) not null,
+    classification_kids bool default false,
+    category enum('Eletrônico','Vestimenta','Brinquedo','Livro','Alimento','Móveis') not null,
+    evaluation float default 0, 
+    size varchar(10),
+    constraint unique_cpf_client unique (CPF)
+);
+
+
+-- tabela pedido
+create table productorder (
+	idOrder int auto_increment primary key,
+    idOrderClient int,
+    orderStatus enum('Cancelado','Confirmado','Em processamento') default 'Em processamento' not null,
+    orderDescription varchar(255),
+    sendValue float default 10,
+	paymentCash bool default false,
+    constraint fk_orders_client foreign key (idOrderClient) references clients(idClient),
+     constraint fk_client_payment foreign key (idpayment) references client_payment(idpayment)
+);
+
+-- tabela estoque
+create table productstorage (
+	idProdStorage int auto_increment primary key,
+    storage_location varchar (255) not null,
+    quantify int default 0
+    );
+    
+-- tabela fornecedor
+create table supplier (
+	idsupplier int auto_increment primary key,
+    social_name varchar (255) not null,
+    CNPJ char(15) not null,
+    contact char(11) not null,
+    constraint unique_supplier unique (CNPJ)
+    );
+    
+-- tabela vendedor
+create table seller (
+	idseller int auto_increment primary key,
+    social_name varchar (255) not null,
+    abs_name varchar(255),
+    CNPJ char(15) not null,
+    contact char(11) not null,
+    location varchar(255),
+    constraint unique_cnpj_seller unique (CNPJ),
+    constraint unique_cpf_seller unique (CPF)
+    );
+
+-- tabela produtos/vendedor
+create table productseller (
+	idPseller int,
+    idProduct int,
+    prod_quantify int default 1,
+    primary key (idPseller, idProduct),
+    constraint fk_product_seller foreign key (idPSeller) references seller(idSeller),
+    constraint fk_product_product foreign key (idProduct) references seller(idproduct)
+);
+
+create table product_order(
+	idPOproduct int,
+    idPOordert int, 
+    po_quantify int default 1,
+    po_status enum ('Disponível', 'Em falta') default 'Disponível',
+    primary key (idPOproduct, idPOorder),
+    constraint fk_productorder_seller foreign key (idPOpodruct) references product(idProduct),
+    constraint fk_productorder_product foreign key (idPOorder) references productorder(idOrder)
+);
+
+create table storagelocation (
+	idLproduct int,
+    idLstorage int,
+    location varchar (255) not null,
+    primary key (idLproduct,idLstorage),
+    constraint fk_storage_location_product foreign key (idLproduct) references product (idProduct),
+    constraint fk_storage_location_storage foreign key (idLproduct) references productstorage (idProdstorage)
+    );
