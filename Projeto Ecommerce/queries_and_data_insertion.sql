38209a0e1da973c8c3f52f7a0d25666da9c44f4e
Fernanda Dias <112582162+fernandadiasm@users.noreply.github.com>
2022-09-12 19:42:36 -0300

    adiciona algumas persistências de dados

--- Projeto Ecommerce/queries_and_data_insertion.sql
+++ Projeto Ecommerce/queries_and_data_insertion.sql
@@ -4,49 +4,101 @@ use ecommerce;
 
 show tables;
 
--- id client, Fname, Minit, LName, CPF, Address
-insert into Clients (email, first_name, middle_name, last_name, CPF, date_of_birthday, gender, phone_number)
-			values('fernandadias.bio@hotmail.com','Fernanda','Dias','Ferraz','123456789','1997-06-27','F','123456701'),
-                  ('ramomferraz@hotmail.com','Ramom','Ferraz','Dias','123227789','1995-04-27','M','123456711'),
-                  ('michael@hotmail.com','Michael','Flores','Assis','223456789','1951-01-13','M','123456910'),
-                  ('Julia@hotmail.com','Julia','França','Monteiro','333456789','2000-12-25','F','125678910'),
-                  ('maria@hotmail.com','Maria','Dias','Menezes','883456789','1952-08-22','F','345678910');
+-- clients
+insert into clients (email,phone_number,address_line1,address_line2,postal_code,city,country) values
+				('fernandadias.bio@hotmail.com','123456701','Rua A','número 36','21220222','Rio de Janeiro','Brasil'),
+				('ramomferraz@hotmail.com','123456910','Rua A','número 36','21223322','Rio de Janeiro','Brasil'),
+				('maria@hotmail.com','345678910','Rua D',null,'21220782','Santa Catarina','Brasil');
+        
+        
+        
+-- physical_clients
+insert into  physical_clients(first_name,middle_name,last_name,CPF,date_of_birthday,gender) values
+			       ('Fernanda','Dias','Menezes','12345678910','1997-06-27','F'),
+                               ('Ramom','Ferraz','Dias','12345678654','1900-04-27','M'),
+                               ('Maria',null,'Borges','98746372850','2000-08-22','F');
+ select * from physical_clients; 
+ 
+ 
+ 
+ -- legal_clients
+insert into legal_clients(social_name,CNPJ) values
+				('Empresa MillaSakura LTDA','984756389281736'),
+				('Companhia das Letras','184756398756402'),
+				('Rocco Jovens Leitores','194857643890');
+select * from legal_clients;
 
-select * from clients;   
-                  
--- idaddress, id client, address_line1, address_line2, postal_code,city,country
-insert into client_address (idClient, address_line1, address_line2, postal_code, city, country) values	
-							('Rua A','número 36','21220222','Rio de Janeiro','Brasil'),
-                            ('Rua A','número 36','21223322','Rio de Janeiro','Brasil'),
-                            ('Rua B','número 4','212456722','São Paulo','Brasil'),
-                            ('Rua C','número 7','21220852','Rio das Ostras','Brasil'),
-                            ('Rua D','número 15','21220782','Santa Catarina','Brasil');
 
-delete from client_address
-where idAddress =10;
 
-select * from client_address; 
+-- clients_payment
+insert into clients_payment (payment_type, payment_value) values
+				('Boleto','100.00'),
+                                (default,'59.99'),
+                                ('Pix','559.25');
+select * from clients_payment; 
 
--- idpayment, idclient, payment_type, payment_value
-insert into client_payment ( payment_type, payment_value) values
-							('Boleto','13,90'),
-                            (null,'50,00'),
-                            ('Pix','6000,90'),
-                            ('Cartão','10,20'),
-                            ('Dois cartões','999,99');
-                            
-select * from client_payment; 
-                            
+
+
+-- product
 insert into product (name_product, classification_kids, category, product_value, evaluation, size) values
-					('Fone de ouvido', false,'Eletrônico','100.00','4',null),
-                    ('Barbie', true,'Brinquedo','59.99','3',null),
-                    ('Body Carters', true, 'Vestimenta','39.99','5',null),
-                    ('As crônicas de gelo e fogo', false, 'Livro','15.50','5',null),
-                    ('Sofá retrátil', false,'Móveis','559.25','2','3x57x80');
-                    
+				('Fone de ouvido', false,'Eletrônico','100.00','4',null),
+				('Barbie', true,'Brinquedo','59.99','3',null),
+				('Sofá retrátil', false,'Móveis','559.25','2','3x57x80');                    
 select * from product; 
-insert into orders (idOrderclient, orderStatus, orderdescription, sedValue, paymentCash) values
-					(1,null,'Compra via aplicativo', null, 1),
-                    (2,null,'compra via aplicativo',50,0),
-                    (3,'Confirmado',null,null,1),
-                    (4,null,'compra via web site',150,0);
+
+
+
+-- orders
+insert into orders (idOrderclient, order_status, order_description, send_value, payment_cash) values
+				(1,default,'Compra via aplicativo', null, 1),
+				(2,default,'compra via Web site',50,0),
+				(3,'Confirmado',null,null,1);
+select * from orders;      
+          
+          
+-- product_order
+insert into product_order (idProductOrder, idPOorder, po_status, po_quantify) values
+			 (1,1,'Indisponível',0),
+                         (2,1,'Disponível',1),
+                         (3,2,Default,5);
+                    
+ select * from product_order;                    
+                    
+-- productstorage
+insert into productstorage (storage_location, quantify) values
+			    ('Rio de Janeiro',1000),
+                            ('São Paulo',200),
+                            ('Minas Gerais','10');
+   select * from productstorage; 
+   
+   
+
+-- storagelocation
+insert into storagelocation (idLproduct, idLstorage, location) values
+			    (1,2,'RJ'),
+                            (2,3,'GO');
+ select * from storagelocation;                            
+                            
+     
+     
+-- supplier
+insert into supplier (social_name,CNPJ,contact) values
+			    ('Almeida e filhos','948576028195738','21098765789'),
+                            ('Eletrônicos Silva','678576028195738','21096765789'),
+                            ('Eletrônicos Balma','942346028195738','21098955789');
+   select * from supplier;                            
+                            
+                     
+-- seller
+insert into seller(social_name, CNPJ, contact,location) values
+			    ('Eletrônicos e cia','123456789098764','21987643829','RJ'),
+                            ('Livros e cia','128473820964738','21983457067','RJ'),
+                            ('Móveis e cia','928374021946578','21973456891','RJ');
+  select * from seller; 
+
+
+-- productseller
+insert into productseller (idPseller, idProduct, prod_quantify) values
+			    (1,6,80),
+                            (2,1,10);
+  select * from productseller; 
